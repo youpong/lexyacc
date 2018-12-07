70aed957df5d61f698b5cc643dc39cb506e55819
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-12-07 20:54:34 +0900

    add main[012].c

--- 0-0/main2.c
+++ 0-0/main2.c
@@ -7,20 +7,16 @@
 int cnt;
 
 int main(int argc, char *argv[]) {
-
-  for (int i = 1; i < argc; i++) {
+  for(int i = 1; i < argc; i++) {
     FILE *file = fopen(argv[i], "r");
-    if (file == NULL) {
-      fprintf(stderr, "file(%s) cannot open.\n", argv[i]);
-      continue;
+    if(file == NULL) {
+      fprintf(stderr, "file cannot open\n");
+      exit(EXIT_FAILURE);
     }
     yyin = file;
-
-    cnt = 0;
     yyparse();
-    fclose(yyin);
     printf("\n");
   }
-
+  
   return EXIT_SUCCESS;
 }
--- 0-0/test_func.c
+++ 0-0/test_func.c
@@ -2,4 +2,4 @@
 #include "test.h"
 #include <CUnit/CUnit.h>
 
-void test_func_000(void) { CU_ASSERT(0); }
+void test_func_000(void) { CU_ASSERT(1); }
