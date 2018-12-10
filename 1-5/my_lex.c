868f0e8fb01400410ffae040a69f215ae95e2b71
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-12-10 18:37:14 +0900

    delete needless top while loop.

--- 1-5/my_lex.c
+++ 1-5/my_lex.c
@@ -18,96 +18,94 @@ int yylex(void) {
   char *p;
   yytext = (char *)malloc(sizeof(char) * 100);
 
-  while (true) {
-    p = yytext;
+  p = yytext;
 
-    /* [ \t]+ ;ignore white space */
-    while ((c = getc(yyin)) == ' ' || c == '\t')
-      ;
+  /* [ \t]+ ;ignore white space */
+  while ((c = getc(yyin)) == ' ' || c == '\t')
+    ;
 
-    if (c == EOF)
-      return 0;
+  if (c == EOF)
+    return 0;
 
-    /* number(1 of 2)
-     * [0-9]+         |
-     * [0-9]+\.[0-9]+ |
-     */
-    if (isdigit(c)) {
+  /* number(1 of 2)
+   * [0-9]+         |
+   * [0-9]+\.[0-9]+ |
+   */
+  if (isdigit(c)) {
+    *p++ = c;
+    while ((c = getc(yyin)) != EOF && isdigit(c))
+      *p++ = c;
+    if (c == '.') {
+      *p++ = c;
+      if ((c = getc(yyin)) == EOF || !isdigit(c)) {
+        ungetc(c, yyin);
+        ungetc(*--p, yyin);
+        *p = '\0';
+        return NUMBER;
+      }
       *p++ = c;
       while ((c = getc(yyin)) != EOF && isdigit(c))
         *p++ = c;
-      if (c == '.') {
-        *p++ = c;
-        if ((c = getc(yyin)) == EOF || !isdigit(c)) {
-          ungetc(c, yyin);
-          ungetc(*--p, yyin);
-          *p = '\0';
-          return NUMBER;
-        }
-        *p++ = c;
-        while ((c = getc(yyin)) != EOF && isdigit(c))
-          *p++ = c;
-      }
-      ungetc(c, yyin);
-      *p = '\0';
-      return NUMBER;
     }
+    ungetc(c, yyin);
+    *p = '\0';
+    return NUMBER;
+  }
 
-    /* number(2 of 2)
-     * \.[0-9]+ reutnr NUMBER;
-     */
-    if (c == '.') {
-      int type = c;
+  /* number(2 of 2)
+   * \.[0-9]+ reutnr NUMBER;
+   */
+  if (c == '.') {
+    int type = c;
+    *p++ = c;
+    while ((c = getc(yyin)) != EOF && isdigit(c)) {
+      type = NUMBER;
       *p++ = c;
-      while ((c = getc(yyin)) != EOF && isdigit(c)) {
-        type = NUMBER;
-        *p++ = c;
-      }
-      ungetc(c, yyin);
-      *p = '\0';
-      return type;
     }
+    ungetc(c, yyin);
+    *p = '\0';
+    return type;
+  }
 
-    /* #.* return COMMENT */
-    if (c == '#') { /* comment */
-      while ((c = getc(yyin)) != EOF && c != '\n')
-        ;
-      ungetc(c, yyin);
-      return COMMENT;
-    }
+  /* #.* return COMMENT */
+  if (c == '#') { /* comment */
+    while ((c = getc(yyin)) != EOF && c != '\n')
+      ;
+    ungetc(c, yyin);
+    return COMMENT;
+  }
 
-    /* \"[^"\n]*\" return TEXT */
+  /* \"[^"\n]*\" return TEXT */
+  if (c == '"') {
+    *p++ = c;
+    while ((c = getc(yyin)) != EOF && c != '"' && c != '\n')
+      *p++ = c;
     if (c == '"') {
       *p++ = c;
-      while ((c = getc(yyin)) != EOF && c != '"' && c != '\n')
-        *p++ = c;
-      if (c == '"') {
-        *p++ = c;
-        *p = '\0';
-        return TEXT;
-      }
-
-      /*
-       * '"' で閉じられていないので、'"'以後読み込んだデータを積み直して
-       * 最初からやり直す
-       */
-      *p++ = c;
-      while (p - yytext > 1) {
-        ungetc(*--p, yyin);
-      }
-
-      return yytext[0];
+      *p = '\0';
+      return TEXT;
     }
 
-    /* check to see if it is a command */
-    if (isalpha(c)) {
-      while ((c = getc(yyin)) != EOF && isalnum(c))
-        ;
-      ungetc(c, yyin);
-      return COMMAND;
+    /*
+     * '"' で閉じられていないので、'"'以後読み込んだデータを積み直して
+     * 最初からやり直す
+     */
+    *p++ = c;
+    while (p - yytext > 1) {
+      ungetc(*--p, yyin);
     }
 
-    /* . ECHO; gleaning */
-    return c;
+    return yytext[0];
   }
+
+  /* check to see if it is a command */
+  if (isalpha(c)) {
+    while ((c = getc(yyin)) != EOF && isalnum(c))
+      ;
+    ungetc(c, yyin);
+    return COMMAND;
+  }
+
+  /* . ECHO; gleaning */
+  return c;
 }
