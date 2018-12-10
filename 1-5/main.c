36d43a25c58bdf3322c2dcad39b32904c914dbca
NAKAJIMA Yusaku <yupong7@gmail.com>
2018-12-10 18:06:33 +0900

    clang-formatted.

--- 1-5/main.c
+++ 1-5/main.c
@@ -5,16 +5,21 @@
 
 int main(int argc, char *argv[]) {
   int val;
-  char *word_type[] = { "NUMBER", "COMMENT", "TEXT", "COMMAND", };
-  FILE *file ;
-  
-  switch(argc) {
+  char *word_type[] = {
+      "NUMBER",
+      "COMMENT",
+      "TEXT",
+      "COMMAND",
+  };
+  FILE *file;
+
+  switch (argc) {
   case 1:
     yyin = stdin;
     break;
   case 2:
     file = fopen(argv[1], "r");
-    if(file == NULL) {
+    if (file == NULL) {
       fprintf(stderr, "file cannot open\n");
       exit(EXIT_FAILURE);
     }
@@ -24,14 +29,14 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
     exit(EXIT_FAILURE);
   }
-  
+
   while ((val = yylex()) != 0) {
-    if(val >= NUMBER && val <= COMMAND )
+    if (val >= NUMBER && val <= COMMAND)
       printf("%s", word_type[val - NUMBER]);
     else
       printf("%d", val);
-    
-    if(val == NUMBER || val == TEXT) {
+
+    if (val == NUMBER || val == TEXT) {
       printf("(%s)", yytext);
     }
     printf("\n");
--- 1-5/word.h
+++ 1-5/word.h
@@ -4,4 +4,3 @@ typedef enum {
   TEXT,
   COMMAND,
 } WORD_TYPE;
-
